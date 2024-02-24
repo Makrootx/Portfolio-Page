98c7075efe69fcb17bb6776f93f198cf6e829161
Makrootx <maksym782@gmail.com>
2024-02-25 00:19:39 +0100

    Supported changing theme of website
    Added a possibility of changing theme of the website. Also added custom heading font.

--- pages/_app.js
+++ pages/_app.js
@@ -1,18 +1,12 @@
 import { ChakraProvider } from "@chakra-ui/provider";
 import Layout from '../components/layouts/main'
-import { extendTheme} from "@chakra-ui/react";
-import theme from "../libs/theme";
-
-const myTheme=extendTheme({
-    config:{
-        initialColorMode: 'dark',
-        useSystemColorMode: true
-    }
-})
+import theme from "../lib/theme";
+import Fonts from "../components/fonts";
 
 const Website=({Component, pageProps, router})=>{
     return(
         <ChakraProvider theme={theme}>
+            <Fonts/>
             <Layout router={router}>
                 <Component {... pageProps} key={router.route}/>
             </Layout>
