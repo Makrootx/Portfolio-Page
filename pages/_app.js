8260b8deb469e2e660cfbc1af5d80b9bb620fd5a
Makrootx <maksym782@gmail.com>
2024-02-26 20:17:58 +0100

    Added works page
    Added a works page and a placeholder for works.

--- pages/_app.js
+++ pages/_app.js
@@ -2,11 +2,13 @@ import { ChakraProvider } from '@chakra-ui/provider'
 import Layout from '../components/layouts/main'
 import theme from '../lib/theme'
 import Fonts from '../components/fonts'
+import { GridItemStyle } from '../components/grid-item'
 
 const Website = ({ Component, pageProps, router }) => {
     return (
         <ChakraProvider theme={theme}>
             <Fonts />
+            <GridItemStyle />
             <Layout router={router}>
                 <Component {...pageProps} key={router.route} />
             </Layout>
---
+++ pages/works.js
@@ -0,0 +1,36 @@
+import { Container, Heading, SimpleGrid, Divider } from '@chakra-ui/react'
+import Section from '../components/section'
+import { WorkGridItem } from '../components/grid-item'
+import thumbCalculator from '../public/images/projectThumbSample.png'
+import thumbJavaGame from '../public/images/projectThumbSample.png'
+const Works = () => {
+    return (
+        <Container>
+            <Heading as="h3" fontSize={20} mb={4}>
+                Works
+            </Heading>
+            <SimpleGrid columns={[1, 1, 2]} gap={6}>
+                <Section>
+                    <WorkGridItem
+                        id="calculator"
+                        title="Calculator AI"
+                        thumbnail={thumbCalculator}
+                    >
+                        A calculator with AI implamentation of recognizing
+                        mathematical symbols
+                    </WorkGridItem>
+                </Section>
+                <Section>
+                    <WorkGridItem
+                        id="javagame"
+                        title="Java Game"
+                        thumbnail={thumbJavaGame}
+                    >
+                        A 2D Platformer Game writed on Java
+                    </WorkGridItem>
+                </Section>
+            </SimpleGrid>
+        </Container>
+    )
+}
+export default Works
