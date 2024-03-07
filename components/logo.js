f7be4eb70fe260f79372935d0d80250a426145f0
Makrootx <maksym782@gmail.com>
2024-03-07 21:37:05 +0100

    Logo updated

--- components/logo.js
+++ components/logo.js
@@ -1,6 +1,6 @@
 import Link from 'next/link'
 import Image from 'next/image'
-import { Text, useColorModeValue } from '@chakra-ui/react'
+import { Text, useColorModeValue, Box } from '@chakra-ui/react'
 
 import styled from '@emotion/styled'
 
@@ -23,18 +23,20 @@ const LogoBox = styled.span`
 `
 
 const Logo = () => {
-    const footPrintImg = `/images/myLogo${useColorModeValue('', '-dark')}.png`
+    const footPrintImg = `/images/logo-${useColorModeValue('dark', 'light')}.png`
 
     return (
         <Link href="/">
             <LogoBox>
-                <Image
-                    src={footPrintImg}
-                    width={20}
-                    height={20}
-                    alt="logo"
-                    style={{ width: 'auto', height: 'auto' }}
-                />
+                <Box mt={-1}>
+                    <Image
+                        src={footPrintImg}
+                        width={21}
+                        height={20}
+                        alt="logo"
+                        // style={{ width: 'auto', height: 'auto' }}
+                    />
+                </Box>
                 <Text
                     color={useColorModeValue('gray.800', 'whiteAlpha.900')}
                     fontFamily="M PLUS Rounded 1c"
